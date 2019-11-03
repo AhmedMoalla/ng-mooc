5a8d0eaa88f2dd10c06dd1fd7cd302a8ad919563
Ahmed Moalla <ahmed.moalla@esprit.tn>
2019-11-03 15:34:56 +0100

    Removed AppComponent content and AppComponent selector from index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
+    // Default module to import for application running in the browser
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
