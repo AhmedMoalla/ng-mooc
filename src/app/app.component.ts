1eab26eeae996f9dd2c3dc181c0e4c0f4d93bfc0
Ahmed Moalla <ahmed.moalla@esprit.tn>
2019-11-03 15:40:54 +0100

    Added AppComponent code, add its selector to index.html, updated AppModule

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello, I am the first component</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Ng-Mooc';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     // Default module to import for application running in the browser
     BrowserModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
