539d2b4e49c7a85805a271b0c4e1199beaa60538
Ahmed Moalla <ahmed.moalla@esprit.tn>
2019-11-03 16:28:32 +0100

    Added CourseListComponent and added toolbar to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     // Default module to import for application running in the browser
-    BrowserModule
+    BrowserModule,
+
+    // Angular Material Components
+    MatToolbarModule, // <mat-toolbar></mat-toolbar>
+    MatIconModule, // <mat-icon></mat-icon>
+    MatButtonModule // mat-button
   ],
   providers: [],
   bootstrap: [AppComponent]
