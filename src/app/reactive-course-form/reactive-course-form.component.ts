5c7ec03e070809a8cabf9131771306944adca6d6
Ahmed Moalla <ahmed.moalla@esprit.tn>
2019-11-04 22:02:36 +0100

    Made the rangeValidator parametrable

--- src/app/reactive-course-form/reactive-course-form.component.ts
+++ src/app/reactive-course-form/reactive-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-course-form',
@@ -19,16 +19,18 @@ export class ReactiveCourseFormComponent implements OnInit {
       image: ['', [Validators.required, Validators.pattern(this.urlRegex)]], // This is equivalent to the line above
       category: this.fb.control('', [Validators.required, Validators.minLength(3)]),
       description: this.fb.control('', [Validators.maxLength(100)]),
-      price: this.fb.control(0, [Validators.required, this.rangeValidator]),
-      rating: this.fb.control(0, [Validators.required, this.rangeValidator])
+      price: this.fb.control(0, [Validators.required, this.createRangeValidator(0, 100)]),
+      rating: this.fb.control(0, [Validators.required, this.createRangeValidator(0, 5)])
     });
   }
 
-  rangeValidator(control: AbstractControl): ValidationErrors | null {
-    if (control.value < 0 || control.value > 5) {
-      return { range: { min: 0, max: 5 } };
-    }
-    return null;
+  createRangeValidator(min: number, max: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value < min || control.value > max) {
+        return { range: { min: min, max: max } };
+      }
+      return null;
+    };
   }
 
   save() {
